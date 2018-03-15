a07bb73db6305926f5f66dabeb435fa1033ee838
Justin Pfeifler <justin.pfeifler@revunit.com>
2018-03-15 16:11:17 -0500

    add all

---
+++ Assets/Scripts/BallObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallObject : MonoBehaviour {
+
+	public AudioSource audioSource;
+
+	void Start(){
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.relativeVelocity.magnitude > .01)
+			audioSource.Play ();
+	}
+
+	/**
+	void onCollisionEnter (Collision col) {
+		print ("Detected collision btwn" + gameObject.name + " and " + col.collider.name);
+		Debug.Log ("GAME OBJECT" + col.gameObject.name);
+		if (col.gameObject.name == "Ball") {
+			Debug.Log ("Play audio here");
+		}
+	}
+
+	void Awake() {
+		print ("Ball object script woke");
+	}
+	*/
+
+}
---
+++ Assets/Scripts/ViveControllerInputTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveControllerInputTest : MonoBehaviour
+{
+
+	private SteamVR_TrackedObject trackedObj;
+
+	private SteamVR_Controller.Device Controller {
+		get { return SteamVR_Controller.Input ((int)trackedObj.index); }
+	}
+
+	void Awake ()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// Get the position of the finger when it's on the touchpad
+		if (Controller.GetAxis () != Vector2.zero) {
+			Debug.Log (gameObject.name + Controller.GetAxis ());
+		}
+
+		// Trigger Down
+		if (Controller.GetHairTriggerDown ()) {
+			Debug.Log (gameObject.name + " Trigger Press");
+		}
+
+		// Trigger Release
+		if (Controller.GetHairTriggerUp ()) {
+			Debug.Log (gameObject.name + " Trigger Release");
+		}
+
+		// Grip Press
+		if (Controller.GetPressDown (SteamVR_Controller.ButtonMask.Grip)) {
+			Debug.Log (gameObject.name + "Grip Press");
+		}
+
+		// Grip press release
+		if (Controller.GetPressUp (SteamVR_Controller.ButtonMask.Grip)) {
+			Debug.Log (gameObject.name + " Grip Release");
+		}
+	}
+}
